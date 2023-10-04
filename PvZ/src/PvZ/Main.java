afde206fb7023c9802c32202926dd333e6110d8d
Vlad <vlad.filiuk2006@gmail.com>
2023-10-04 14:18:24 -0500

    added background image

--- PvZ/src/PvZ/Main.java
+++ PvZ/src/PvZ/Main.java
@@ -5,30 +5,31 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main {
-
 	public static void main(String[] args) {
 		JFrame frame, winframe;
 		JButton[] menu = new JButton[4];
 		JButton[][] board = new JButton[5][9];
 		JPanel contentPane;
+		
 		frame = new JFrame("PvZ");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(1000, 700);
 		GridBagConstraints c = new GridBagConstraints();
 		frame.setLocationRelativeTo(null);
 		contentPane = new JPanel(new GridBagLayout());
-		ImageIcon imageTile,imagePeaShooter,imageWallNut,imagePotatoMine;
+		ImageIcon imageTile,imagePeaShooter,imageWallNut,imagePotatoMine,imageBackground;
 		imagePeaShooter=new ImageIcon("res//PeaShooter.png");
+		imageBackground=new ImageIcon("res//backround.png");
 		imageWallNut=new ImageIcon("res//WallNut.png");
 		imagePotatoMine=new ImageIcon("res//PotatoMine.png");
 		imageTile = new ImageIcon("res//R.png");
-	//	String[][] a = { { "00", "10", "20", "30", "40", "50", "60", "70" },
-	//			{ "01", "11", "21", "31", "41", "51", "61", "71" }, { "02", "12", "22", "32", "42", "52", "62", "72" },
-	//			{ "03", "13", "23", "33", "43", "53", "63", "73" }, { "04", "14", "24", "34", "44", "54", "64", "74" },
-	//			{ "05", "15", "25", "35", "45", "55", "65", "75" }, { "06", "16", "26", "36", "46", "56", "66", "76" },
-	//			{ "07", "17", "27", "37", "47", "57", "67", "77" } };
+	 
+
+	//	setLayout(new BorderLayout);
+		c.gridheight=6;
+		c.gridwidth=10;
+	contentPane.add( new JLabel(imageBackground),c); 
 		c.gridy=0;
-	
 		for (int i = 0; i < menu.length; i++) {
 			c.gridx = i;
 			menu[i] = new JButton();
@@ -36,6 +37,9 @@ public class Main {
 			menu[i].setPreferredSize(new Dimension(90, 90));	
 			
 			contentPane.add(menu[i], c);
+			c.gridheight=1;
+			c.gridwidth=1;
+
 		}
 	
 		menu[0].setIcon(imagePeaShooter);
@@ -43,8 +47,11 @@ public class Main {
 		menu[2].setIcon(imagePotatoMine);
 		
 		for (int i = 0; i < board.length; i++) {// create board
+		 	
 			c.gridy = i+1;
 			for (int j = 0; j < board[0].length; j++) {
+				c.gridheight=1;
+				c.gridwidth=1;
 				board[i][j] = new JButton();
 				board[i][j].setPreferredSize(new Dimension(90, 90));
 				c.gridx = j;
@@ -55,5 +62,7 @@ public class Main {
 		}
 		frame.setContentPane(contentPane);
 		frame.setVisible(true);
-}
+
+	}
+
 }
