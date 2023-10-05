c354d8ec1575f50c98e813650a9dfee4027714a1
Kinzie F <kinziefriesen47@gmail.com>
2023-10-05 11:54:19 -0500

    Created peashooter class.

---
+++ PvZ/src/PvZ/Peashooter.java
@@ -0,0 +1,29 @@
+//Kinzie F
+//Oct 5, 2023
+//This is a class for the peashooter plant in the PvZ simulation
+
+package PvZ;
+
+public class Peashooter {
+	private int psHealth;
+	private int  rateOfFire;
+	
+	public Peashooter() {
+		psHealth = 200;
+		rateOfFire = 2; //every two seconds
+	}
+	
+	/**
+	 * method for getting health
+	 */
+	public int getHealth() {
+		return psHealth;
+	}
+	
+	/**
+	 * method for shooting bullet
+	 */
+	public int shootBullet() {
+		
+	}
+}
