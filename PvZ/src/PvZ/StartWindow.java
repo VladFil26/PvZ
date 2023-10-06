6adb99339b0b574f6bc563d3143f6fb1f7826c03
Vlad <vlad.filiuk2006@gmail.com>
2023-10-06 12:15:17 -0500

    moved some code to UI class

--- PvZ/src/PvZ/StartWindow.java
+++ PvZ/src/PvZ/StartWindow.java
@@ -1,31 +1,32 @@
 package PvZ;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+public class StartWindow {
 
-/*public class StartWindow {
 	JFrame instructionframe;
-	instructionframe=new JFrame("guide");
-	instructionframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	instructionframe.setSize(500, 300);
+	JPanel contentPane2 = new JPanel(new GridBagLayout());
 	GridBagConstraints c2 = new GridBagConstraints();
-	contentPane2 = new JPanel(new GridBagLayout());
-		 instructionframe.setContentPane(contentPane2);
-	 	instructionframe.setVisible(true);
-	 	JLabel label1=new JLabel("Enter zombies limit");
-		JTextField  tbox1=new JTextField(5);
-		JButton button1=new JButton("Start");
-		c2.gridy=0;
-		contentPane2.add(label1,c2);
-		c2.gridy=1;
-		contentPane2.add(tbox1,c2);
-c2.gridy=2;
-contentPane2.add(button1,c2);
+	JButton button1 = new JButton("Start");
+	JTextField tbox1 = new JTextField(5);
+
+	public StartWindow() {
+		instructionframe = new JFrame("guide");
+		instructionframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		instructionframe.setSize(500, 300);
+		instructionframe.setLocationRelativeTo(null);
+		JLabel label1 = new JLabel("Enter zombies limit");
+		c2.gridy = 0;
+		contentPane2.add(label1, c2);
+		c2.gridy = 1;
+		contentPane2.add(tbox1, c2);
+		c2.gridy = 2;
+		contentPane2.add(button1, c2);
+		instructionframe.setContentPane(contentPane2);
+		instructionframe.setVisible(true);
+
+	}
+
 }
-*/
--- PvZ/src/PvZ/UI.java
+++ PvZ/src/PvZ/UI.java
@@ -1,5 +1,116 @@
 package PvZ;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class UI {
+	int choise;
+	JFrame frame = new JFrame("PvZ");
+	JPanel contentPane = new JPanel(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
+	public JButton[][] board = new JButton[5][9];
+	public JButton[] menu = new JButton[4];
+	ImageIcon imageTile, imagePeaShooter, imageWallNut, imagePotatoMine, imageBackground, imageZombie, image2Zombies,
+			imageShovel;
+	String[][] a = { { "00", "10", "20", "30", "40", "50", "60", "70", "80" },
+			{ "01", "11", "21", "31", "41", "51", "61", "71", "81" },
+			{ "02", "12", "22", "32", "42", "52", "62", "72", "85" },
+			{ "03", "13", "23", "33", "43", "53", "63", "73", "83" },
+			{ "04", "14", "24", "34", "44", "54", "64", "74", "84" }, };
+	String[] b = { "0", "1", "2", "3" };
+	// setLayout(new BorderLayout);
+	// c.gridheight=6;
+	// c.gridwidth=10;
+	// contentPane.add( new JLabel(imageBackground),c);
+
+	public UI() {
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 700);
+		frame.setLocationRelativeTo(null);
+		imagePeaShooter = new ImageIcon("res//PeaShooter.png");
+		imageBackground = new ImageIcon("res//backround.png");
+		imageWallNut = new ImageIcon("res//WallNut.png");
+		imagePotatoMine = new ImageIcon("res//PotatoMine.png");
+		imageTile = new ImageIcon("res//R.png");
+		imageZombie = new ImageIcon("res//ZombieR.png");
+		image2Zombies = new ImageIcon("res//2ZombieR.png");
+		imageShovel = new ImageIcon("res//shovel.png");
+		c.gridy = 0;
+		for (int i = 0; i < menu.length; i++) {
+			c.gridx = i;
+			menu[i] = new JButton();
+			menu[i].setPreferredSize(new Dimension(90, 90));
+			contentPane.add(menu[i], c);
+			// c.gridheight=1;
+			// c.gridwidth=1;
+
+		}
+		menu[0].setIcon(imagePeaShooter);
+		menu[1].setIcon(imageWallNut);
+		menu[2].setIcon(imagePotatoMine);
+		menu[3].setIcon(imageShovel);
+
+		for (int i = 0; i < board.length; i++) {// create board
+			c.gridy = i + 1;
+			for (int j = 0; j < board[0].length; j++) {
+				// c.gridheight=1;
+				// c.gridwidth=1;
+				board[i][j] = new JButton();
+				board[i][j].setPreferredSize(new Dimension(90, 90));
+				c.gridx = j;
+				board[i][j].setIcon(imageTile);
+				contentPane.add(board[i][j], c);
+			}
+		}
+		for (int i = 0; i < menu.length; i++) {
+			menu[i].setActionCommand(b[i]);
+			menu[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					int x = Integer.valueOf(e.getActionCommand());
+					// System.out.println(x);
+					choise = x;
+				}
+			});
+		}
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[0].length; j++) {
+				board[i][j].setActionCommand(a[i][j]);
+				board[i][j].addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int y = Integer.valueOf(e.getActionCommand().substring(0, 1)); // -current index
+						int x = Integer.valueOf(e.getActionCommand().substring(1));// -current index
+						if (y != board[0].length - 1) {
+							switch (choise) {
+							case 0:
+								if (board[x][y].getIcon() == imageTile)
+									board[x][y].setIcon(imagePeaShooter);
+								break;
+							case 1:
+								if (board[x][y].getIcon() == imageTile)
+									board[x][y].setIcon(imageWallNut);
+								break;
+							case 2:
+								if (board[x][y].getIcon() == imageTile)
+									board[x][y].setIcon(imagePotatoMine);
+								break;
+							case 3:
+								if (board[x][y].getIcon() != imageTile)
+									board[x][y].setIcon(imageTile);
+								break;
+							default:
+								choise = -1;
+								break;
+							}
+							choise = -1;
+						}
+					}
+				});
+			}
+		}
+		frame.setContentPane(contentPane);
+		frame.setVisible(true);
+	}
 
 }
