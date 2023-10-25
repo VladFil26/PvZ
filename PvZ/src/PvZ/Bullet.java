f6f8a31949fac03078b098872c5e03542ee83c31
Vlad <vlad.filiuk2006@gmail.com>
2023-10-25 11:17:36 -0500

    added image of bullet with zombie

--- PvZ/src/PvZ/Bullet.java
+++ PvZ/src/PvZ/Bullet.java
@@ -10,109 +10,109 @@ public class Bullet {
 	static int health;
 	static int damage;
 	static int speed;
-	int x, y; 
-		
-		public Bullet() {
-			Random r = new Random(); 
-			health = 200;
-			damage = -100;
-			speed = 1000; 
-			x = r.nextInt(0, 4);
-			y = 9;
-		}
-		
-		/**
-		 * Method for getting health
-		 */
-		public int getHealth() {
-			return health;
-		}
-		
-		/**
-		 * Method to get damage it can do
-		 */
-		public int getDamage(){
-			return damage;
-		}
-		
-		/**
-		 * method to get speed
-		 */
-		public int getSpeed() {
-			return speed;
-		}
-		
-		/**
-		 * Method for resetting health
-		 */
-		public int resetbHealth() {
-			health = 200;
-			return health;
-		}
-		
-		public int getX() {
-		return x;	
-		}
-		
-		public int getY() { 
-			return y;
-		}
-		
-		/**
-		 * Method for shooting zombies
-		 **/
-		public void shootZombie(Zombie[] zombies, Peashooter[] peashooters, PotatoMine[] potatomines, Wallnut[] wallnuts) {
-			boolean t = false;
-			
-			for(int i = 0; i < zombies.length; i++) {
-				if(zombies[i].getX() == x && zombies[i].getY() == y) {
-					if(zombies[i].getHealth() > 0) {
-						zombies[i].setHealth(damage);
-						
-					}else { 
-				}
-				
-				
-			}
-			
-		
-			for (int i = 0; i < peashooters.length; i++) {
-				if (peashooters[i].getX() == x && peashooters[i].getY() == y) { //checking peashooters array with same x and y
-					if (peashooters[i].getHealth() > 0) {
-						peashooters[i].setHealth(damage);
-					} else {
-						peashooters[i].setY(10);
-					}
-					t = true;
-					//System.out.println("Eating peashooter");
-				}
-			}
+	int x, y;
 
-			for (int i = 0; i < potatomines.length; i++) { //checking potatomines array with same x and y
-				if (potatomines[i].getX() == x && potatomines[i].getY() == y) {
-					if (potatomines[i].getHealth() > 0) {
-						potatomines[i].setHealth(damage);
-					} else
-						potatomines[i].setY(10);
-					t = true;
-					//System.out.println("Eating potatomines");
-				}
-			}
-			for (int i = 0; i < wallnuts.length; i++) { //checking wallnuts array with same x and y
-				if (wallnuts[i].getX() == x && wallnuts[i].getY() == y) {
-					if (wallnuts[i].getHealth() > 0) {
-						wallnuts[i].setHealth(damage);
-					} else {
-						wallnuts[i].setY(10);
-					}
-					t = true;
-					//System.out.println("Eating wallnuts");
-				}
-			}
+	public Bullet() {
+		Random r = new Random();
+		health = 200;
+		damage = -100;
+		speed = 1000;
+		x = r.nextInt(0, 4);
+		y = 9;
+	}
 
-			if (!t && y >= 1) {
-				// System.out.println("move");
-				y++;
+	/**
+	 * Method for getting health
+	 */
+	public int getHealth() {
+		return health;
+	}
+
+	/**
+	 * Method to get damage it can do
+	 */
+	public int getDamage() {
+		return damage;
+	}
+
+	/**
+	 * method to get speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+
+	/**
+	 * Method for resetting health
+	 */
+	public int resetbHealth() {
+		health = 200;
+		return health;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	/**
+	 * Method for shooting zombies
+	 **/
+	public void shootZombie(Zombie[] zombies, Peashooter[] peashooters, PotatoMine[] potatomines, Wallnut[] wallnuts) {
+		boolean t = false;
+
+		for (int i = 0; i < zombies.length; i++) {
+			if (zombies[i].getX() == x && zombies[i].getY() == y) {
+				if (zombies[i].getHealth() > 0) {
+					zombies[i].setHealth(damage);
+
+				} else {
+				}
+
 			}
 		}
+
+		for (int i = 0; i < peashooters.length; i++) {
+			if (peashooters[i].getX() == x && peashooters[i].getY() == y) { // checking peashooters array with same x
+																			// and y
+				if (peashooters[i].getHealth() > 0) {
+					peashooters[i].setHealth(damage);
+				} else {
+					peashooters[i].setY(10);
+				}
+				t = true;
+				// System.out.println("Eating peashooter");
+			}
+		}
+
+		for (int i = 0; i < potatomines.length; i++) { // checking potatomines array with same x and y
+			if (potatomines[i].getX() == x && potatomines[i].getY() == y) {
+				if (potatomines[i].getHealth() > 0) {
+					potatomines[i].setHealth(damage);
+				} else
+					potatomines[i].setY(10);
+				t = true;
+				// System.out.println("Eating potatomines");
+			}
+		}
+		for (int i = 0; i < wallnuts.length; i++) { // checking wallnuts array with same x and y
+			if (wallnuts[i].getX() == x && wallnuts[i].getY() == y) {
+				if (wallnuts[i].getHealth() > 0) {
+					wallnuts[i].setHealth(damage);
+				} else {
+					wallnuts[i].setY(10);
+				}
+				t = true;
+				// System.out.println("Eating wallnuts");
+			}
+		}
+
+		if (!t && y >= 1) {
+			// System.out.println("move");
+			y++;
+		}
+	}
 }
